4f04544d15ebfa4991e8a6665c8ee942295bbb66
Alberto Toledo <albtolmay@alum.us.es>
2021-12-18 13:44:45 +0100

    feat : Crear página base #7
    Primera aproximación de la página base

--- decide/decide/urls.py
+++ decide/decide/urls.py
@@ -17,12 +17,14 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
+from base.views import InicioView
 
 
 schema_view = get_swagger_view(title='Decide API')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', InicioView.as_view()),
+    path('admin/', admin.site.urls, name="admin"),
     path('doc/', schema_view),
     path('gateway/', include('gateway.urls')),
 ]

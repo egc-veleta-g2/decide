fb80305ae2d9b09a1c8e6979c3e296f3c7b50b15
Jesusjbs <jesbarsig@alum.us.es>
2021-12-19 16:44:29 +0100

    feat :  Crear interfaz de la cabina de votacion #11
    Implementada la interfaz de la cabina de votación,
    falta la integración con el inicio de sesión.

--- decide/booth/urls.py
+++ decide/booth/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import BoothView
+from .views import BoothView, InicioView
+from booth import views
 
 
 urlpatterns = [
     path('<int:voting_id>/', BoothView.as_view()),
+    path('inicio/', InicioView.as_view()),
 ]
--- decide/booth/views.py
+++ decide/booth/views.py
@@ -4,6 +4,10 @@ from django.conf import settings
 from django.http import Http404
 
 from base import mods
+from store.models import Vote
+from voting.models import Voting
+from census.models import Census
+
 
 
 # TODO: check permissions and census
@@ -29,3 +33,36 @@ class BoothView(TemplateView):
         context['KEYBITS'] = settings.KEYBITS
 
         return context
+
+
+class InicioView(TemplateView):
+    template_name = 'booth/inicio.html'
+
+    
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['listVotados']= self.get_queryset()[0]
+        context['listNoVotados']= self.get_queryset()[1]
+        context['usuario'] = self.request.user
+
+        return context
+
+    
+    def get_queryset(self):
+        votaciones = Voting.objects.all()
+        mis_votos = Vote.objects.filter(voter_id= self.request.user.id)
+        mis_censos = Census.objects.filter(voter_id= self.request.user.id)
+
+        list_votados = set()
+        for v in votaciones:
+            for voto in mis_votos:
+                if voto.voting_id == v.id:
+                    list_votados.add(v)
+        
+        list_noVotados = []
+        for v in votaciones:
+            for c in mis_censos:
+                if v.id == c.voting_id and v not in list_votados:
+                    list_noVotados.append(v)
+
+        return list_votados, list_noVotados

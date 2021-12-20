00bf0ff80f10b51a7777ba5cfeacd46bd6de6814
Jesusjbs <jesbarsig@alum.us.es>
2021-12-20 17:18:24 +0100

    feat : Implementar test con selenium #11
    Creado test con selenium para validar la vista como
    usuario no registrado

--- decide/booth/urls.py
+++ decide/booth/urls.py
@@ -5,5 +5,5 @@ from booth import views
 
 urlpatterns = [
     path('<int:voting_id>/', BoothView.as_view()),
-    path('inicio/', InicioView.as_view()),
+    path('inicio/', InicioView.as_view(), name="boothInicio"),
 ]

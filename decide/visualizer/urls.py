85d5190eec5ac56b8d59a216a79d48cb7115cf48
Alberto Toledo <albtolmay@alum.us.es>
2021-12-20 20:33:37 +0100

    fix : Arreglar enlace visualización en inicio #19
    Añadido nombre al archivo urls y modificado el html de inicio

--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -3,6 +3,6 @@ from .views import VisualizerView, VisualizerInicioView
 
 
 urlpatterns = [
-    path('', VisualizerInicioView.as_view()),
+    path('', VisualizerInicioView.as_view(), name="visualizer_inicio"),
     path('<int:voting_id>/', VisualizerView.as_view(), name="visualizer"),
 ]

3ef7e6dfe4d4e0a9ce93e60c2aa2b4abcc5f79ec
manmogvil <manmogvil@alum.us.es>
2022-01-03 12:25:20 +0100

    Update docker-settings.py

--- docker/docker-settings.py
+++ docker/docker-settings.py
@@ -5,7 +5,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD':'postgres',
+        'PASSWORD':'complexpassword',
         'HOST': 'db',
         'PORT': 5432,
     }

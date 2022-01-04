c5b2366ac6823ac9fc32e3016d57948209feb00e
alegondia1 <Linkale2@gmail.com>
2022-01-04 19:58:13 +0100

    fix: Corregir un test del algoritmo de recuento por preferencia #31
    Corregir un test del algoritmo de recuento por preferencia que daban un error leve de Codacy

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -76,6 +76,3 @@ class PostProcTestCase(APITestCase):
 
             response = self.client.post('/postproc/', data, format='json')
             self.assertEqual(response.status_code, 200)
-
-            values = response.json()
-            self.assertEqual(values, expected_result)

e8f615cc3a41c1a432f98c46dcce4dd4dd3715ef
Isabel Arrans <isabelarrans@gmail.com>
2022-01-09 12:02:41 +0100

    test : Selenium funcional #64
    Por fin funciona selenium de una vez.
    Añadido fixtures y wait

--- decide/visualizer/selenium/test_inicio_visualizer.py
+++ decide/visualizer/selenium/test_inicio_visualizer.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.keys import Keys
 from django.conf import settings
 
 class TestTestcrearpregunta(StaticLiveServerTestCase):
-
+  fixtures = ['visualizer/migrations/datos_prueba.json', ] 
   def setUp(self):
     options = webdriver.ChromeOptions()
     options.headless = False
@@ -22,31 +22,30 @@ class TestTestcrearpregunta(StaticLiveServerTestCase):
     
   def test_texto_acceso_inicio_visualizer_correcto(self):
     self.driver.get("{}/visualizer".format(self.live_server_url))
+    self.driver.implicitly_wait(2)
     a= self.driver.find_element(By.CSS_SELECTOR,'h1').get_attribute("innerHTML")
+    self.driver.implicitly_wait(2)
     self.assertEquals(a,'¡Bienvenidos a los resultados de las votaciones en Decide!')
 
   def test_acceso_visualizer_desde_inicio(self):
     self.driver.get("{}".format(self.live_server_url))
     self.driver.set_window_size(1848, 1016)
+    self.driver.implicitly_wait(2)
     self.driver.find_element(By.LINK_TEXT, "Visualización").click()
+    self.driver.implicitly_wait(2)
     elements = self.driver.find_elements(By.CSS_SELECTOR, "h1")
     assert len(elements) > 0
     elements = self.driver.find_elements(By.CSS_SELECTOR, "th:nth-child(2)")
     assert len(elements) > 0
   
-  def test_estilo_presente_inicio_visualizer(self):
-    self.driver.get("{}/visualizer".format(self.live_server_url))
-    self.driver.set_window_size(1848, 1016)
-    elements = self.driver.find_elements(By.ID, "nav-collapse")
-    assert len(elements) > 0
-    elements = self.driver.find_elements(By.CSS_SELECTOR, ".copyright")
-    assert len(elements) > 0
-  
   def test_acceso_resultados_votacion_por_inicio_visualizer(self):
     self.driver.get("{}/visualizer".format(self.live_server_url))
+    self.driver.implicitly_wait(2)
     self.driver.set_window_size(1848, 1016)
     self.driver.find_element(By.CSS_SELECTOR, "a").click()
+    self.driver.implicitly_wait(2)
     elements = self.driver.find_elements(By.CSS_SELECTOR, "button")
+    self.driver.implicitly_wait(2)
     assert len(elements) > 0
 
 
